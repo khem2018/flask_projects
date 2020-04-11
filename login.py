2493b88a4f14589af8bcb3a2b9a6928da2e5c059
Khem Sharma <43295209+khem2018@users.noreply.github.com>
2020-04-11 19:05:25 +0530

    Adding updated login.py

--- login.py
+++ login.py
@@ -3,12 +3,13 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/authenticate_user',methods=["POST"])
+@app.route('/authenticate_user',methods=["GET","POST"])
 def authenticate_user():
-    fname = request.get('fname')
-    lname = request.get('lname')
+    fname = request.form('fname')
+    lname = request.form('lname')
     full_name = fname + lname
+    import pdb;pdb.set_trace()
     return render_template('test.html',name = full_name)
 
 if __name__=='__main__':
-    app.run(host ='0.0.0.0',port='4000',debug=True)
+    app.run(port=5500,debug=True)
